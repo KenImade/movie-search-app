e572ba343d43d8d393f695d25d48488171c96e47
Kenneth Imade <kenneth.imade@yahoo.com>
2022-05-11 13:30:32 +0100

    added card component to display movie info

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,62 @@
-
+import {useState} from "react";
 
 function SearchBar() {
+
+    const [query, setQuery] = useState("");
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (e) => {
         e.preventDefault()
         console.log("submitting...")
-
-        const query = "dr strange"
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=76a7bc1729a83da0d730d73e873a4de6&
                     language=en-US&query=${query}&page=1&include_adult=false`;
 
         try {
             const res = await fetch(url);
-            const data = await res.json(); 
+            const data = await res.json();
+            setMovies(data.results)
             console.log(data);
         } catch (error) {
             console.error(error)
-        } 
+        }
+        
+        setQuery("")
     }
 
     
 
     return (
-        <form className="form" onSubmit={searchMovies}>
-            <label htmlFor="query" className="label">Search Movies</label>
-            <input name="query" placeholder="i.e Multiverse of Madness..." type="text" className="input"/>
-            <button type="submit" className="button">Search</button>
-        </form>
+        <>
+            <form className="form" onSubmit={searchMovies}>
+                <label htmlFor="query" className="label">Search Movies</label>
+                <input 
+                    name="query" 
+                    placeholder="i.e Multiverse of Madness..." 
+                    type="text" 
+                    className="input"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type="submit" className="button">Search</button>
+            </form>
+            <div className="card--list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                    <div className="card" key={movie.id}>
+                        <img className="card--img"
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                            alt={movie.title + "poster"}
+                        />
+                        <div className="card--content">
+                            <h3 className="card--title">{movie.title}</h3>
+                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                            <p><small>RATING: {movie.vote_average}</small></p>
+                            <p className="card--desc">{movie.overview}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
 
