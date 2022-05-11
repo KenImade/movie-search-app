927a4b2d3513bd3da696f89da5b6ad4322bf6558
Kenneth Imade <kenneth.imade@yahoo.com>
2022-05-11 12:41:08 +0100

    Create the Search Movies Function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,30 @@
 
 function SearchBar() {
 
+    const searchMovies = async (e) => {
+        e.preventDefault()
+        console.log("submitting...")
+
+        const query = "dr strange"
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=76a7bc1729a83da0d730d73e873a4de6&
+                    language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json(); 
+            console.log(data);
+        } catch (error) {
+            console.error(error)
+        } 
+    }
+
+    
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label htmlFor="query" className="label">Search Movies</label>
-            <input name="query" placeholder="search for movie..." type="text" className="input"/>
+            <input name="query" placeholder="i.e Multiverse of Madness..." type="text" className="input"/>
             <button type="submit" className="button">Search</button>
         </form>
     )
